206f91dba7ed4df9137cb02bf7f75463e7b84843
Luke Kruyer <kruyerl@gmail.com>
2021-02-07 00:13:02 -0500

    reinit

--- server/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '47ad262ff33debfca84ec50a3e78da7f'),
-    },
-  },
-});
--- server/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'aed6d830-8398-4b45-a683-c38565548c3b'
-};
